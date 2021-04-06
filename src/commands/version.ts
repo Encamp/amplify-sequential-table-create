146625f0d1833286d75d38b6aacb49c7d32783ef
Daniel Smedema <dan@encamp.com>
2021-04-06 15:35:26 -0400

    directory structure

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,5 +1,4 @@
 export async function run(context) {
   // print out the version of your plugin package
-  const { version } = await import("../package.json");
-  context.print.info(version);
+  context.print.info("1.0.0");
 }
